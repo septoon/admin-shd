8fefbbd02fbfc2ac025d70216b3e09ab79d306ab
Tigran <41443306+septoon@users.noreply.github.com>
2024-10-14 15:08:27 +0300

    change routes

--- src/App.js
+++ src/App.js
@@ -2,44 +2,34 @@ import React, { useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import WebApp from '@twa-dev/sdk';
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 
-import Menu from './assets/img/menu.png'
-import Contacts from './assets/img/contacts.png'
-import Delivery from './assets/img/delivery.png'
-import Vacancy from './assets/img/vacancy.png'
+import Menu from './components/Menu/Menu';
+import Contacts from './components/Contacts/Contacts';
+import Delivery from './components/Delivery/Delivery';
+import Vacancies from './components/Vacancies/Vacancies';
+import Main from './components/Main/Main';
+
 
 function App() {
-
   useEffect(() => {
     WebApp.ready();
     WebApp.expand();
-  }, [])
+  }, []);
 
   const linkClassName = 'w-[80%] h-20 flex justify-start pl-5 items-center mb-5 bg-silver dark:bg-darkGray rounded-lg';
 
   return (
     <div className="p-2 w-full h-full flex flex-col justify-start items-center bg-gray-100 min-h-screen">
       <ToastContainer />
-      <h1 className="text-3xl font-bold text-center mb-16">Админ панель</h1>
-
-      <Link to="/menu" className={linkClassName}>
-        <img src={Menu} className="h-5 mr-3" alt="kitchen" />
-        <span className="dark:text-white">Меню</span>
-      </Link>
-      <Link to="/contacts" className={linkClassName}>
-        <img src={Contacts} className="h-5 mr-3" alt="bar" />
-        <span className="dark:text-white">Контакты</span>
-      </Link>
-      <Link to="/delivery" className={linkClassName}>
-        <img src={Delivery} className="h-5 mr-3" alt="grill" />
-        <span className="dark:text-white">Доставка</span>
-      </Link>
-      <Link to="/vacancies" className={linkClassName}>
-        <img src={Vacancy} className="h-5 mr-3" alt="household" />
-        <span className="dark:text-white">Вакансии</span>
-      </Link>
-      <Outlet />
+      
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/delivery" element={<Delivery />} />
+        <Route path="/vacancies" element={<Vacancies />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,40 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import Menu from './components/Menu/Menu';
-import Contacts from './components/Contacts/Contacts';
-import Delivery from './components/Delivery/Delivery';
-import Vacancies from './components/Vacancies/Vacancies';
-import Layout from './components/Layout';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
-import { PrimeReactProvider } from 'primereact/api';
-import 'primereact/resources/themes/lara-light-cyan/theme.css';
-import 'primeicons/primeicons.css';
-
-import NotFound from './components/NotFound';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <App /> }, // Главная страница с навигацией
-      { path: 'menu', element: <Menu /> },
-      { path: 'contacts', element: <Contacts /> },
-      { path: 'delivery', element: <Delivery /> },
-      { path: 'vacancies', element: <Vacancies /> },
-    ]
-  }
-]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <PrimeReactProvider>
-      <RouterProvider router={router} />
-    </PrimeReactProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
